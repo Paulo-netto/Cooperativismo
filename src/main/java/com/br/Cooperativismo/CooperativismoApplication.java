df466ed42fe814acf3ce36027ee25b0c934d4786
Paulo Martins <pmon.ims@gmail.com>
2020-11-07 23:28:08 -0300

    First commit

---
+++ src/main/java/com/br/Cooperativismo/CooperativismoApplication.java
@@ -0,0 +1,13 @@
+package com.br.Cooperativismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativismoApplication.class, args);
+	}
+
+}
